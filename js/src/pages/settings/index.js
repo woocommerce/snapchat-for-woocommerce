cc458a9998faa4ed294cc2c4423e106ece75429f
asvinb <asvin.balloo@gmail.com>
2025-07-08 17:55:41 +0400

    Check for a connected/disconnected account.

--- js/src/pages/settings/index.js
+++ js/src/pages/settings/index.js
@@ -1,14 +1,30 @@
 /**
+ * External dependencies
+ */
+import { useEffect } from '@wordpress/element';
+import { getHistory } from '@woocommerce/navigation';
+
+/**
  * Internal dependencies
  */
 import useMenuEffect from '~/hooks/useMenuEffect';
 import LinkedAccounts from './linked-accounts';
 import MainTabNav from '~/components/main-tab-nav';
+import useSnapchatAccount from '~/hooks/useSnapchatAccount';
+import { getOnboardingUrl } from '~/utils/urls';
 import './index.scss';
 
 const Settings = () => {
 	// Make the component highlight SFW entry in the WC legacy menu.
 	useMenuEffect();
+
+	const { isConnected, hasFinishedResolution } = useSnapchatAccount();
+
+	useEffect( () => {
+		if ( ! isConnected && hasFinishedResolution ) {
+			getHistory().replace( getOnboardingUrl() );
+		}
+	}, [ isConnected, hasFinishedResolution ] );
 
 	return (
 		<div className="sfw-settings">
