bcbd3d1d1f5e0400e9041aad79596240080892bf
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2025-07-31 17:03:55 +0530

    improve PAGE_VIEW E2E tests

--- tests/e2e/plugins/snapchat-options.php
+++ tests/e2e/plugins/snapchat-options.php
@@ -8,7 +8,7 @@ use SnapchatForWooCommerce\Utils\Storage\TransientDefaults;
 use SnapchatForWooCommerce\Utils\Storage\Transients;
 
 if ( class_exists( Options::class ) && defined( 'E2E_CONTEXT' ) ) {
-	Transients::set( TransientDefaults::PIXEL_SCRIPT, '<script>var x="https://sc-static.net/scevent.min.js";window.snaptr=function(){window.snaptr.queue.push(Array.from(arguments))},window.snaptr.queue=[],snaptr("track","PAGE_VIEW");</script>' );
+	Transients::set( TransientDefaults::PIXEL_SCRIPT, '<script>var x="https://sc-static.net/scevent.min.js";window.snaptr=function(){window.snaptr.queue.push(Array.from(arguments))},window.snaptr.queue=[];</script>' );
 }
 
 /**
--- tests/e2e/specs/tracking/pixels/page_view-event.spec.js
+++ tests/e2e/specs/tracking/pixels/page_view-event.spec.js
@@ -14,7 +14,9 @@ test.describe( 'PAGE_VIEW event', () => {
 	const PAGES = [
 		{ url: '/', name: 'Home Page' },
 		{ url: '/shop', name: 'Shop Page' },
-		{ url: '/product/product-one', name: 'Single Product Page' },
+		{ url: '/sample-page', name: 'Sample Page' },
+		{ url: '/my-account', name: 'My Account Page' },
+		{ url: '/cart', name: 'Cart Page' },
 	];
 
 	const themes = getThemes();
@@ -29,5 +31,13 @@ test.describe( 'PAGE_VIEW event', () => {
 				expect( PAGE_VIEW ).not.toBe( null );
 			} );
 		}
+
+		test( `[${ theme } themme] Does not send on Single Product page`, async ( { page } ) => {
+			await switchTheme( page, themes[ theme ] );
+			await page.goto( '/product/product-two' );
+			const events = await page.evaluate( () => window.snaptr.queue );
+			const PAGE_VIEW = findSnaptrEvent( events, 'PAGE_VIEW' );
+			expect( PAGE_VIEW ).toBe( null );
+		} );
 	}
 } );
