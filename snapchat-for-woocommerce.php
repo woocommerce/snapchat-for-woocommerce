e635e743ce0fbb64b1dad8f531ebbb1fa00d8519
Ankit K <ankit.himcs@gmail.com>
2025-09-08 21:00:09 +0530

    Wooo Version Bump

--- snapchat-for-woocommerce.php
+++ snapchat-for-woocommerce.php
@@ -10,8 +10,8 @@
  * Requires Plugins: woocommerce
  * Requires PHP: 7.4
  * Requires at least: 6.7
- * WC requires at least: 9.9
- * WC tested up to: 10.1
+ * WC requires at least: 10.0
+ * WC tested up to: 10.2
  *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
